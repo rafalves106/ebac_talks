dc5effbc65a8331dcf5c23cdad3dc7a528029416
rafalves106 <rafalves106@icloud.com>
2024-07-15 23:20:08 -0300

    Criando a contagem regressiva

---
+++ dist/index.5e7e2e50.js
@@ -0,0 +1,2 @@
+AOS.init();const e=new Date("Dec 12, 2022 19:00:00").getTime(),o=setInterval(function(){let t=e-new Date().getTime(),n=Math.floor(t/864e5),l=Math.floor(t%864e5/36e5),a=Math.floor(t%36e5/6e4),c=Math.floor(t%6e4/1e3);console.log(n),console.log(l),console.log(a),console.log(c),document.getElementById("contador").innerHTML=`${n}d ${l}h ${a}m ${c}s`,t<0&&(clearInterval(o),document.getElementById("contador").innerHTML="Evento expirado")},1e3);
+//# sourceMappingURL=index.5e7e2e50.js.map
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -585,6 +585,30 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"3cYfC":[function(require,module,exports) {
 AOS.init();
+const dataDoEvento = new Date("Dec 12, 2022 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const segundoEmMs = 1000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutoEmMs / segundoEmMs);
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "Evento expirado";
+    }
+}, 1000);
 
 },{}]},["8bDoD","3cYfC"], "3cYfC", "parcelRequire4566")
 
